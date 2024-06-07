4e97d99af08168d009277849450d2d98e050e9c5
Elisha Bwilukiro <bwilukiroelisha@gmail.com>
2024-06-07 17:43:37 +0300

    Use login form create with validation. Ref #2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -105,11 +105,6 @@
 	<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 	<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 
-
-	{{-- <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script> --}}
-	{{-- <script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script> --}}
-	{{-- <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script> --}}
-	<!--Password show & hide js -->
 	<script>
 		$(document).ready(function () {
 
